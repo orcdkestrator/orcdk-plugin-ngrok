20282ec1fe84094bbe572481db534183b2ef4dd7
listenrightmeow <mike@coada.dev>
2025-08-05 01:24:29 -0700

    fix: add missing test file for NgrokPlugin

---
+++ src/__tests__/NgrokPlugin.test.ts
@@ -0,0 +1,12 @@
+import { NgrokPlugin } from '../index';
+
+describe('NgrokPlugin', () => {
+  it('should be defined', () => {
+    expect(NgrokPlugin).toBeDefined();
+  });
+
+  it('should have correct name', () => {
+    const plugin = new NgrokPlugin();
+    expect(plugin.name).toBe('ngrok');
+  });
+});
